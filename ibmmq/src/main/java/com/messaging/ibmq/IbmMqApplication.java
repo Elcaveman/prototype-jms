0487e68384dceb461fa58935e7f6aa5adaa355cd
elcaveman <02.oudaoud@gmail.com>
2024-01-30 02:04:44 +0100

    Maven fixes

--- ibmmq/src/main/java/com/messaging/ibmq/IbmMqApplication.java
+++ ibmmq/src/main/java/com/messaging/ibmq/IbmMqApplication.java
@@ -1,5 +1,8 @@
-package com.messaging.rabbitmq;
+package com.messaging.ibmq;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IbmMqApplication {
--- rabbitmq/src/main/java/com/messaging/rabbitmq/application/RabbitMqListener.java
+++ rabbitmq/src/main/java/com/messaging/rabbitmq/application/RabbitMqListener.java
@@ -1,5 +1,7 @@
 package com.messaging.rabbitmq.application;
 
+import org.springframework.stereotype.Component;
+
 @Component
 public class RabbitMqListener {
 
